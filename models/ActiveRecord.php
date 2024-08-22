b95b56d4b8d7a802bd7fd16ad28300b39662b08a
cristian23190225 <dsm23190225.cbarraza@alumnos.utsv.edu.mx>
2024-08-22 10:40:11 -0600

    debuger en la funcion queri

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -143,6 +143,7 @@ class ActiveRecord {
         $query .= " ) VALUES ('"; 
         $query .= join("', '", array_values($atributos));
         $query .= "') ";
+        debuguear($query);
 
         // Resultado de la consulta
         $resultado = self::$db->query($query);
